c21f9bc730d001f41876f2ea0ef211a3f9e60a36
rk134 <me@rk134.cf>
2023-02-11 21:02:57 +0000

    lunaa: Terminate call DEVICE_SKU function
    Signed-off-by: rk134 <me@rk134.cf>

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ $(foreach DEVICE_SKU, $(QCV_FAMILY_SKUS), \
         $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_mixer_paths.xml \
         $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/sound_trigger_mixer_paths.xml \
         $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_platform_info.xml \
-        $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/sound_trigger_platform_info.xml
+        $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/sound_trigger_platform_info.xml)
 
 PRODUCT_PACKAGES += \
     libstdc++.vendor
