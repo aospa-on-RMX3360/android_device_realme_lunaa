82012c52a8f6490746eeedf767f877e142bc15bd
rk134 <me@rk134.cf>
2023-02-15 19:12:36 +0100

    Revert "oneplus9: Include SEPolicy from hardware/oplus."
    This reverts commit 92d6e4da98ae92ad5848d9e5285db6193567b00f.
    
    Signed-off-by: rk134 <me@rk134.cf>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,9 @@ SOONG_CONFIG_ufsbsg += ufsframework
 SOONG_CONFIG_ufsbsg_ufsframework := bsg
 
 # SELinux
-include hardware/oplus/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Touch
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
